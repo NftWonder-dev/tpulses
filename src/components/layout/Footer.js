e1a1ce94725f1dc80dee71d5e48fd1c2851cca22
NftWonder-dev <nftwonder@gmail.com>
2026-09-24 15:49:28 +0100

    youtube link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -32,7 +32,7 @@ export default function Footer() {
               <Youtube className="w-4 h-4" />
             </a>
             <a
-              href="#"
+              href="https://www.youtube.com/@TRIMPulses"
               className="p-2 bg-white/5 rounded-full hover:bg-cyan-500/20 hover:text-cyan-400 transition-colors"
             >
               <Twitter className="w-4 h-4" />
